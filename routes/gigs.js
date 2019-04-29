6eb00480be51b56c5aadaf731000ad50f9939ca3
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-29 01:55:08 -0400

    Uses Gig.findAll() statement to return all records from the PostgreSQL database

--- routes/gigs.js
+++ routes/gigs.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../config/database');
+const Gig = require('../models/Gig');
 
-router.get('/', (req, res) => res.send('GIGS'));
+router.get('/', (req, res) => {
+    // res.send('GIGS');
+
+    Gig.findAll()
+        .then(gigs => {
+            console.log(gigs);
+            res.sendStatus(200);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+
+});
 
 module.exports = router;
