213117af602ccd8030a0cd800a035b0e00835eb8
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-05-05 15:23:42 -0400

    Adds comments to first Route in gigs.js

--- routes/gigs.js
+++ routes/gigs.js
@@ -4,9 +4,10 @@ const db = require('../config/database');
 const Gig = require('../models/Gig');
 
 router.get('/', (req, res) => {
+    // Test route with:
     // res.send('GIGS');
 
-    Gig.findAll()
+    Gig.findAll() // <- returns a promise, so much use .then() and .catch() statements
         .then(gigs => {
             console.log(gigs);
             res.sendStatus(200);
