46c5c5d7b6cf8015f37d0ddf433ded172130ceae
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-05-05 15:11:09 -0400

    Modifies console.log statement for .then() statement in db.authenticate() statement

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const db = require('./config/database');
 // Tests DB
 db.authenticate()
     .then(() => {
-        console.log('Connection has been established successfully.');
+        console.log('Connection to PostgreSQL DB has been established successfully.');
     })
     .catch(err => {
         console.error('Unable to connect to the database:', err);
