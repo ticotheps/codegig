4af19de0d6359ce104d20342e01bdcc46658d986
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-05-06 13:59:16 -0400

    Creates an app.use() statement to enable Body Parser in app.js file

--- app.js
+++ app.js
@@ -23,6 +23,9 @@ const app = express();
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // Set static folder (public)
 app.use(express.static(path.join(__dirname, 'public')));
 
