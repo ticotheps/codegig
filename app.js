a0614749efd5f55215a895738260f3d7af72e5ad
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-05-06 13:22:01 -0400

    Sets the static folder to be used by app.js by creating an app.use() statement under handlebars middleware in app.js

--- app.js
+++ app.js
@@ -23,6 +23,9 @@ const app = express();
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+// Set static folder (public)
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
     res.send('INDEX');
 });
